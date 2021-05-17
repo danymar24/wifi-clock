72b01d863314f718dec077aec999866c0e94213d
Daniel Rdz <drodriguez@norwex.com>
2021-05-17 13:01:38 -0500

    Changed font color to blue

--- wifi-clock.ino
+++ wifi-clock.ino
@@ -7,6 +7,13 @@
 #include <SmartMatrix.h>
 
 #define COLOR_DEPTH 24                  // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
+#define WHITE   rgb24(0xFF,0xFF,0xFF)
+#define BLACK   rgb24(0x00,0x00,0x00)
+#define CYAN    rgb24(0x00,0xFF,0xFF)
+#define RED     rgb24(0xFF,0x00,0x00)
+#define GREEN   rgb24(0x00,0x00,0xFF)
+#define BLUE    rgb24(0x00,0xFF,0x00)
+
 const uint16_t kMatrixWidth = 64;       // Set to the width of your display, must be a multiple of 8
 const uint16_t kMatrixHeight = 32;      // Set to the height of your display
 const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
@@ -65,16 +72,16 @@ void loop() {
 
   sprintf(txtBuffer, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
   indexedLayer1.setFont(font8x13);
-  indexedLayer1.setIndexedColor(1,{0x00, 0x00, 0xff});
-  indexedLayer1.drawString(0, 0, 1, txtBuffer);
+  indexedLayer1.setIndexedColor(1, BLUE);
+  indexedLayer1.drawString(0, 0, 1, daysOfTheWeek[now.dayOfTheWeek()]);
   indexedLayer1.swapBuffers();
   indexedLayer2.setFont(font8x13);
-  indexedLayer2.setIndexedColor(1,{0x00, 0xff, 0x00});
-  indexedLayer2.drawString(0, 11, 1, daysOfTheWeek[now.dayOfTheWeek()]);
+  indexedLayer2.setIndexedColor(1, BLUE);
+  indexedLayer2.drawString(0, 11, 1, txtBuffer);
   indexedLayer2.swapBuffers();
   sprintf(txtBuffer, "%02d %s %04d", now.day(), monthsOfTheYr[(now.month()-1)], now.year());
   indexedLayer3.setFont(font5x7);
-  indexedLayer3.setIndexedColor(1,{0xff, 0x00, 0x00});
+  indexedLayer3.setIndexedColor(1, BLUE);
   indexedLayer3.drawString(0, 25, 1, txtBuffer); 
   indexedLayer3.swapBuffers();
 
